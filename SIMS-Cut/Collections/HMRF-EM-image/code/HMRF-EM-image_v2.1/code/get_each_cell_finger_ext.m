b369f8c0074e51a414bfdbf603e20e9e576a8972
dwSun <dwsunny@icloud.com>
2022-03-24 18:09:02 +0800

    解决数据保存的问题

--- SIMS-Cut/Collections/HMRF-EM-image/code/HMRF-EM-image_v2.1/code/get_each_cell_finger_ext.m
+++ SIMS-Cut/Collections/HMRF-EM-image/code/HMRF-EM-image_v2.1/code/get_each_cell_finger_ext.m
@@ -7,26 +7,13 @@ function [all_cells_info, all_cells_mean, sep_labeling] = get_each_cell_finger_e
     [separated_img] = Separate_cell(img);
     num_cells = max(separated_img(:));
     sep_labeling = separated_img(:);
-    all_cells_info = {};
-    all_cells_mean = [];
+    all_cells_info = cell(num_cells);
+    all_cells_mean = zeros(num_cells, size(test_samples, 2));
 
     for i = 1:num_cells
-        idx_vector = find(sep_labeling == i);
+        idx_vector = sep_labeling == i;
         cur_cell_info = test_samples(idx_vector, :);
         all_cells_info{i} = cur_cell_info;
         cur_cell_mean = mean(cur_cell_info);
-        all_cells_mean = [all_cells_mean; cur_cell_mean];
-        %    b = bar(cur_cell_mean,0.1);
-        %    saveas(b,['each_cell_finger_print/hela_brdu/',num2str(i),'_finger'],'fig');
+        all_cells_mean(i, :) = cur_cell_mean;
     end
-
-    % D = [];
-    % %D只保存距离矩阵的上三角
-    % s = 100;
-    % %s是mmd中gaussian kernel的sigma
-    % for i=1:num_cells
-    %    for j=i+1:num_cells
-    %        D = [D,MMD(all_cells_info{i},all_cells_info{j},s)];
-    %        disp([i,j]);
-    %    end
-    % end
